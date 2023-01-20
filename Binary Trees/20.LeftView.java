8e6e2e4e235659706d8bd4e36f078a750001b48e
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-01-20 22:12:51 +0530

    Left View

---
+++ Binary Trees/20.LeftView.java
@@ -0,0 +1,22 @@
+// Left View of a Binary Tree
+// Time Complexity : O(N)
+// Space Complexity : O(H)  H ---> Height of tree
+
+
+class Solution{
+    ArrayList<Integer> leftView(Node root){
+      ArrayList<Integer> ans=new ArrayList<Integer>();
+      recursion(root , 0 , ans);
+      return ans;
+    }
+    
+    private void recursion(Node root , int level , ArrayList<Integer>ans){
+        
+        if(root == null) return;
+        if(ans.size() == level){
+            ans.add(root.data);
+        }
+        recursion(root.left , level + 1 , ans);
+        recursion(root.right , level + 1 , ans);
+    }
+}
