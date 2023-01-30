18dbca50addcf2f126950a34e8a54b73426067db
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-01-30 19:15:52 +0530

    Eventual Safe States

---
+++ Graph/25.EventualSafeStates.java
@@ -0,0 +1,40 @@
+// Find Eventual Safe States
+// Time Complexity : O(V+E)+O(N*logN), where V = no. of nodes and E = no. of edges. This is a simple BFS algorithm. Extra O(N*logN) for sorting the safeNodes array, where N is the number of safe nodes.
+// Spacce Complexity : O(N) + O(N) + O(N) ~ O(3N), O(N) for the indegree array, O(N) for the queue data structure used in BFS(where N = no.of nodes), and extra O(N) for the adjacency list to store the graph in a reversed order.
+
+
+class Solution {
+    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
+        List<List<Integer>> adjRev = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adjRev.add(new ArrayList<>());
+        }
+        int indegree[] = new int[V];
+        for (int i = 0; i < V; i++) {
+            // u -> v
+            // v -> u
+            for (int it : adj.get(i)) {
+                adjRev.get(it).add(i);
+                indegree[i]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> safeNodes = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            safeNodes.add(node);
+            for (int it : adjRev.get(node)) {
+                indegree[it]--;
+                if(indegree[it] == 0) q.add(it);
+            }
+        }
+        Collections.sort(safeNodes);
+        return safeNodes;
+    }
+}
