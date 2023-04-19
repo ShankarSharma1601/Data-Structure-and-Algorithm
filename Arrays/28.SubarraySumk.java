8cfd2859b296629f1bfb912c4ac13ab10750041f
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-04-19 21:50:10 +0530

    SubArray Sum Equals K

---
+++ Arrays/28.SubarraySumk.java
@@ -0,0 +1,20 @@
+// SubArray Sum Equals K
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer , Integer> map = new HashMap<>();
+        int sum = 0 , ans = 0;
+        map.put(0 , 1);
+        for(int i = 0;i < nums.length;i++){
+            sum += nums[i];
+            if(map.containsKey(sum - k)){
+                ans +=  map.get(sum - k);
+            }
+            map.put(sum , map.getOrDefault(sum , 0) + 1);
+        }
+        return ans;
+    }
+}
