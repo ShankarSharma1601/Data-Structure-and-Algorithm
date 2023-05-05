bf12a3d3ca28fc880a3d6aee904216593ea4dbf2
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-05-05 09:25:51 +0530

    Search Insert Position

---
+++ Binary Search/5.SearchInsertPosition.java
@@ -0,0 +1,17 @@
+// Search Insert Position
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0 , high = nums.length - 1;
+
+        while(low <= high){
+            int mid = low +  (high - low) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) high = mid - 1;
+            else low = mid + 1;
+        }
+        return low;
+    }
+}
