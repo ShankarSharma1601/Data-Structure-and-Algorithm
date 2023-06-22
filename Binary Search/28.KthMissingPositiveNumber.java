df9e7840fd91d2a286518ec01ec0eeea7eed5b5f
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-06-22 19:53:33 +0530

    Kth Missing Positive Number

---
+++ Binary Search/28.KthMissingPositiveNumber.java
@@ -0,0 +1,18 @@
+// Kth Missing Positive Number
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        int start = 0, end = n-1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] - (mid + 1) < k)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return start + k;
+    }
+}
