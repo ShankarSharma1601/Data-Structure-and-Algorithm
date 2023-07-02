44f088446da31a8a1a91b6490a87ecf7f031eedd
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-07-02 09:33:18 +0530

    Isomorphic Strings

---
+++ Strings/5.IsoMorphicSrtings.java
@@ -0,0 +1,18 @@
+// Isomorphic Strings
+// Time Complexity : O(n)
+// Space Complexity :O(n)
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character , Character> map = new HashMap<>();
+        
+        for(int i = 0;i < s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(map.get(s.charAt(i)) != t.charAt(i)) return false;
+            }
+            else if(map.containsValue(t.charAt(i))) return false;
+            else map.put(s.charAt(i) , t.charAt(i));
+        }
+        return true;
+    }
+}
