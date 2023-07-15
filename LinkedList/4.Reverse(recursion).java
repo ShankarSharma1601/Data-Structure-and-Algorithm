568b73efdc9275495677449ed243a71eeedb04d7
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-07-15 23:34:19 +0530

    Reverse a Linked List

---
+++ LinkedList/4.Reverse(recursion).java
@@ -0,0 +1,13 @@
+// Reverse a Linked List
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
