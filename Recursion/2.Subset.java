02b8e427f07c3c23999c86506cabae79a1ce3918
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-07-31 20:29:49 +0530

    Subset

---
+++ Recursion/2.Subset.java
@@ -0,0 +1,24 @@
+// Subset
+// Power Set
+// Time Complexity : O(2^n)
+// Space Complexity : O(n) for recursion stack
+
+class Solution {
+    List<List<Integer>> ans;
+    public List<List<Integer>> subsets(int[] nums) {
+        ans = new ArrayList<>();
+        solve(nums, new ArrayList<Integer>() , 0);
+        return ans;
+    }
+    private void solve(int nums[] , List<Integer> temp , int ind){
+        if(ind == nums.length){
+            ans.add(new ArrayList<>(temp));
+            return ;
+        }
+        
+        temp.add(nums[ind]);
+        solve(nums , temp , ind + 1);
+        temp.remove(temp.size() - 1);
+        solve(nums , temp , ind+1);
+    }
+}
