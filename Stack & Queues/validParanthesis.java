99b6f36c702ac916cad55432066663e87ec9abb2
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-08-17 23:15:39 +0530

    Valid Paranthesis

---
+++ Stack & Queues/validParanthesis.java
@@ -0,0 +1,17 @@
+// Valid Paranthesis
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(') stack.push(')');
+            else if(ch == '{') stack.push('}');
+            else if(ch == '[') stack.push(']');
+            else if(stack.isEmpty() || stack.pop() != ch) return false;
+        }
+        return stack.isEmpty();
+    }
+}
