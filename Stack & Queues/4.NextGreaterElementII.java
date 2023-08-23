8c64f8059409557bab70ba8e3032496707322e34
ShankarSharma1601 <shankarsharma1601@gmail.com>
2023-08-23 23:34:43 +0530

    Next Greater Element II

---
+++ Stack & Queues/4.NextGreaterElementII.java
@@ -0,0 +1,19 @@
+// Next Greater Element II
+// Time Complexity : O(N)
+// Space Complexity :  O(N)
+
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int ans[] = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 2*nums.length - 1;i>=0;i--){
+            while(!stack.isEmpty() && nums[stack.peek()] <= nums[i%nums.length]){
+                stack.pop();
+            }
+            ans[i%nums.length] = stack.isEmpty() ? -1 : nums[stack.peek()];
+            stack.push(i%nums.length);
+        }
+        return ans;
+    }
+}
